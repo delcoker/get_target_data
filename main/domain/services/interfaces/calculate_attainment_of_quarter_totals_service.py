ad3de8a5ef32d80c0c62e480443a8e7322dc429a
del <kingston.coker@statista.com>
2022-07-20 14:20:40 +0200

    revert
    removes unused services

--- main/domain/services/interfaces/calculate_attainment_of_quarter_totals_service.py
+++
@@ -1,4 +0,0 @@
-class CalculateAttainmentOfQuarterTotalsService:
-    def calculate_attainment_of_quarter_totals(self, dataset):
-        """Calculate ST2 Attainment of Quarter from the currently loaded file."""
-        pass
--- main/domain/services/interfaces/calculate_attainment_table_one_service.py
+++
@@ -1,4 +0,0 @@
-class CalculateAttainmentTableOneService:
-    def calculate_attainment_attainment_table(self, full_file_name: str) -> dict:
-        """Multiply median and product growth from the currently loaded file."""
-        pass
