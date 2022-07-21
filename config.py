87364aa8bb8700650b5831ff32ab79cbc220c8e9
del <kingston.coker@statista.com>
2022-07-21 02:58:02 +0200

    abstract c_model_service

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ from main.domain.services.clean_data_service_impl import CleanDataServiceImpl
 from main.domain.services.contexts.attainment_table_strategy_context import AttainmentTableStrategyContext
 from main.domain.services.extract_data_based_on_number_of_years_service_impl import ExtractDataBasedOnNumberOfYearsServiceImpl
 from main.domain.services.extract_unique_products_service_impl import ExtractUniqueProductsServiceImpl
+from main.domain.services.strategies.modelling_techniques.c_model_service_impl import CModelServiceImpl
 from main.domain.target_data_service_impl import TargetDataServiceImpl
 from main.domain.services.forecast_months_service_impl import ForecastMonthsServiceImpl
 
@@ -13,13 +14,13 @@ clean_data_service = CleanDataServiceImpl()
 unique_products_service = ExtractUniqueProductsServiceImpl()
 filter_data_service = ExtractDataBasedOnNumberOfYearsServiceImpl()
 attainment_table_strategy = AttainmentTableStrategyContext()
-attainment_table_strategy = AttainmentTableStrategyContext()
 forecast_months_service = ForecastMonthsServiceImpl()
+c_model_service = CModelServiceImpl(attainment_table_strategy=attainment_table_strategy,
+                                    forecast_months_service=forecast_months_service)
 
 target_data_service = TargetDataServiceImpl(clean_data_service=clean_data_service,
                                             unique_products_service=unique_products_service,
                                             filter_data_service=filter_data_service,
-                                            attainment_table_strategy=attainment_table_strategy,
-                                            forecast_months_service=forecast_months_service)
+                                            c_model_service=c_model_service)
 
 file_repository = FileRepositoryImpl()
---
+++ main/domain/services/interfaces/modelling_technique_strategy_service.py
@@ -0,0 +1,4 @@
+class ModellingTechniqueStrategyService:
+    def evaluate_model(self, filtered_data, all_products, fe_product_growth):
+        """Process c-modelling steps"""
+        pass
