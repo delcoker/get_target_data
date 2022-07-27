fb06b7c1daadc78d613bee315e6367908031c7fe
del <kingston.coker@statista.com>
2022-07-27 13:19:18 +0200

    fix total revenue issue if value is NaN in dataframe
     if value is NaN in dataframe, browser is unable to serialize

--- main/infrastructure/mappers/target_data_mapper_impl.py
+++ main/infrastructure/mappers/target_data_mapper_impl.py
@@ -1,4 +1,5 @@
 import json
+import math
 
 from main.application.models.responses.view_models.booking import Booking
 from main.application.models.responses.view_models.model_data import ModelData
@@ -20,7 +21,7 @@ class TargetDataMapperImpl(TargetDataMapper):
 
             time_period = TimePeriod(quarter=row['Quarter'], month=row['Month'], month_name=row['Month_Name'], year=row['Year']).__dict__
             total_revenue = row['c_model']
-            if total_revenue is None:
+            if math.isnan(total_revenue) or total_revenue is None:
                 total_revenue = row['ARR']
             product_data = ProductData(group_id=i, group_name=row['Product Name'], total_revenue=total_revenue).__dict__
             model_data = ModelData(median=row['Median'], growth_percent=row['Growth_Percent'], growth_value=['Growth_Value'], median_growth=['Median_Grwoth'], mean_arr=['Mean_ARR']).__dict__
